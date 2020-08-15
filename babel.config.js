00cd53497581ad339618a83fe758c840267308e4
tighug <kapuriko123@gmail.com>
2020-08-15 13:58:37 +0900

    chore: Add babel.config.js

---
+++ babel.config.js
@@ -0,0 +1,15 @@
+export default {
+  presets: [
+    [
+      "@babel/preset-env",
+      {
+        targets: {
+          node: "current",
+        },
+      },
+    ],
+    "@babel/preset-typescript",
+    "@babel/preset-react",
+  ],
+  plugins: ["babel-plugin-styled-components"],
+};
